918ce060241ae1a0fe214a908e0f9b03202bfa65
Shuang <35458530+shuangmw@users.noreply.github.com>
2018-01-21 12:23:37 +0800

    Update learning_python.py

--- learning_python.py
+++ learning_python.py
@@ -17,3 +17,23 @@ def replaceSpace(s):
         else:
             temp += i
     return temp
+
+# 重建二叉树
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+class Solution:
+    def reConstructBinaryTree(self, pre, tin):
+        # write code here
+        if not pre or not tin:
+            return None
+        root = TreeNode(pre.pop(0))
+        index = tin.index(root.val)
+        root.left = self.reConstructBinaryTree(pre, tin[:index])
+        root.right = self.reConstructBinaryTree(pre, tin[index + 1:])
+        return root
+sl = Solution() 
+result = sl.reConstructBinaryTree([1,2,4,7,3,5,6,8], [4,7,2,1,5,3,8,6])
+result.left.left.right.val
