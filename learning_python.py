6b072be5a3241232e81f0de42d301b8f230a16d7
Shuang <35458530+shuangmw@users.noreply.github.com>
2018-12-13 13:54:37 +0800

    Update learning_python.py

--- learning_python.py
+++ learning_python.py
@@ -93,3 +93,21 @@ class Solution:
                 t2 = total
                 i += 1
             return total
+        
+        
+# quick sort
+def quick_sort(array, left, right):
+    if left >= right:
+        return
+    low = left
+    high = right-1
+    key = array[right]
+    while left < right:
+        while left < right and array[left] <= key:
+            left += 1
+        while left < right and array[right] > key:
+            right -= 1
+        array[left], array[right] = array[right], array[left]
+    array[left], key = key, array[left]
+    quick_sort(array, low, left-1)
+    quick_sort(array, left+1, high)
