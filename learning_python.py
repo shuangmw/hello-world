4d0b058659621ebe235824f3172dc291a32ad31d
Shuang <35458530+shuangmw@users.noreply.github.com>
2018-12-20 10:22:49 +0800

    Update learning_python.py

--- learning_python.py
+++ learning_python.py
@@ -4,20 +4,7 @@ def print_diamond(n):
         print ' '*(n-i), '*'*(2*i-1)
     for i in range(n-1,0,-1):
         print ' '*(n-i), '*'*(2*i-1)
-
-
-# 替换空格
-def replaceSpace(s):
-    temp = ''
-    if type(s) != str:
-        return
-    for i in s:
-        if i == ' ':
-            temp += '%20'
-        else:
-            temp += i
-    return temp
-
+        
 
 # 重建二叉树
 class TreeNode:
@@ -26,41 +13,20 @@ class TreeNode:
         self.left = None
         self.right = None
 class Solution:
+    # 返回构造的TreeNode根节点
     def reConstructBinaryTree(self, pre, tin):
-        # write code here
         if not pre or not tin:
             return None
-        root = TreeNode(pre.pop(0))
-        index = tin.index(root.val)
-        root.left = self.reConstructBinaryTree(pre, tin[:index])
-        root.right = self.reConstructBinaryTree(pre, tin[index + 1:])
+        root = TreeNode(pre[0])
+        # 找到中序队列中根节点的下标
+        i = tin.index(pre[0])
+        root.left = self.reConstructBinaryTree(pre[1:i + 1], tin[:i])
+        root.right = self.reConstructBinaryTree(pre[i + 1:], tin[i + 1:])
         return root
 
 sl = Solution() 
 result = sl.reConstructBinaryTree([1,2,4,7,3,5,6,8], [4,7,2,1,5,3,8,6])
 result.left.left.right.val
-
-
-# 两个栈实现一个队列
-class Solution:
-    def __init__(self):
-        self.stackA = []
-        self.stackB = []
-         
-    def push(self, node):
-        # write code here
-        self.stackA.append(node)
-         
-    def pop(self):
-        # return xx
-        if self.stackB:
-            return self.stackB.pop()
-        elif not self.stackA:
-            return None
-        else:
-            while self.stackA:
-                self.stackB.append(self.stackA.pop())
-            return self.stackB.pop()
         
         
 # 跳台阶
@@ -76,7 +42,7 @@ class Solution:
         else:
             return self.jumpFloor(number-1) + self.jumpFloor(number-2)
         
-# 迭代
+# 循环
 class Solution:
     def jumpFloor(self, number):
         # write code here
