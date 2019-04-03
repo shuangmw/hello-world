2c29ea53ad191cdb4f5ce545c4be06e28824b7da
wuchunlin <wuchunlin@pku.edu.cn>
2019-04-04 01:27:50 +0800

    fix format

--- reconstruct_binary_tree.py
+++ reconstruct_binary_tree.py
@@ -5,17 +5,16 @@ class TreeNode:
         self.left = None
         self.right = None
 class Solution:
-    # 返回构造的TreeNode根节点
+    # return root of the constructed tree. 
     def reConstructBinaryTree(self, pre, tin):
         if not pre or not tin:
             return None
         root = TreeNode(pre[0])
-        # 找到中序队列中根节点的下标
-        i = tin.index(pre[0])
-        root.left = self.reConstructBinaryTree(pre[1:i + 1], tin[:i])
-        root.right = self.reConstructBinaryTree(pre[i + 1:], tin[i + 1:])
+        i = tin.index(pre[0])  # index of root in tin.
+        root.left = self.reConstructBinaryTree(pre[1:i+1], tin[:i])
+        root.right = self.reConstructBinaryTree(pre[i+1:], tin[i+1:])
         return root
 
 sl = Solution()
 result = sl.reConstructBinaryTree([1,2,4,7,3,5,6,8], [4,7,2,1,5,3,8,6])
-result.left.left.right.val
+print result.left.left.right.val
