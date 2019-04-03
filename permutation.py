51236d7c285148be1a36743186afd4ccf0983e43
wuchunlin <wuchunlin@pku.edu.cn>
2019-04-04 00:54:34 +0800

    fix format

--- permutation.py
+++ permutation.py
@@ -2,12 +2,14 @@ def permutation(a):
     """string permutation"""
     if not a:
         return []
+
     if len(a) == 1:
         return [a]
+
     result = []
     for i in range(len(a)):
-        for j in permutation(a[:i] + a[i+1:]):
-            result.append(a[i] + j)
+        for item in permutation(a[:i] + a[i+1:]):
+            result.append(a[i] + item)
     return result
 
 
